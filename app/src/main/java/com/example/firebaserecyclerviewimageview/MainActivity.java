281ac1b86f23836015160d8260583dc035a8a2d7
Hridoy Hossain <ridoy.java@gmail.com>
2021-11-12 12:25:20 +0600

    Design and find the id of widget

--- app/src/main/java/com/example/firebaserecyclerviewimageview/MainActivity.java
+++ app/src/main/java/com/example/firebaserecyclerviewimageview/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.firebaserecyclerviewimageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button choseImageButton,saveImageButton;
+    EditText imageName;
+    TextView showAllImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
